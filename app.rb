063af548ccfa3200e12c16192a4a10fb9fb9e048
clifford <clifford@noukod.com>
2019-01-11 21:20:53 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,10 @@ class App < Sinatra::Base
 
   # Define your GET '/' route below and respond with "Hello, World!"
 
+class Application < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
 
 end
